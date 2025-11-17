530ccdac70275aa98da96ca0b4adf51d98a7e5ab
Falaf1 <subinad53@gmail.com>
2025-11-17 19:50:06 +0500

    123

---
+++ Tests/Research.py
@@ -0,0 +1,24 @@
+#1
+
+a = int(input("Введите число от 1 до 100: "))
+counter = 1
+while counter != a + 1:
+   print(f'Мы пришли с миром. {counter} ')
+   counter = counter + 1
+
+#2
+
+# a = int(input("Введите число ,больше 1 : "))
+# counter = a
+# count = 1
+# while count <= a:
+#     counter = counter - 1
+#     count += 1
+# print(f"Сумма первых {a} натуральных чисел равна.")
+
+#3
+#Переписать 1 задание на цикл for
+
+# a = int(input("Введите число от 1 до 100: "))
+# for _ in range(a):
+#     print(f'Мы любим своё дело. {counter} ')

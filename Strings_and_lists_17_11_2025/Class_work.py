11cc467c0f31b44628cc3cb185d6361bd9edaa94
Falaf1 <subinad53@gmail.com>
2025-11-17 21:24:12 +0500

    Классная и практическая работа 17 числа.

--- Strings_and_lists_17_11_2025/Class_work.py
+++ Strings_and_lists_17_11_2025/Class_work.py
@@ -31,40 +31,40 @@ strings-AND_LISTS
 Методы списков
 """
 # Добавление списка
-my_list = [1, 2]
-my_list.append("Мы учим Python")
-print(my_list)
-# Расширение списка
-my_list.extend([1, 2, 3, 4])
-print(my_list)
-# Вставка по индексу
-my_list.insert(0, "Сегодня")
-print(my_list)
-# Удаление объекта
-my_list.remove(1)
-print(my_list)
-# Удаление по индексу
-my_list.pop(0)
-print(my_list)
-# Очистка списка
-my_list.clear()
-print(my_list)
-# Подчёт вхождений элемента в списке
-val = my_list.count(1)
-print(val)
-# Сортировка списка
-my_list1 = [1, 5, 6, 7, 455, 323, 11, 146]
-my_list1.sort(reverse=False)
-print(my_list1)
-# Перевернуть список
-my_list1.reverse()
-print(my_list1)
-
-counter = 0
-while counter < len(my_list1):
-    val = my_list1[counter]
-    if val == 323:
-        print("Мы нашли число 323")
-        break
-    print(my_list1[counter])
-    counter += 1
+# my_list = [1, 2]
+# my_list.append("Мы учим Python")
+# print(my_list)
+# # Расширение списка
+# my_list.extend([1, 2, 3, 4])
+# print(my_list)
+# # Вставка по индексу
+# my_list.insert(0, "Сегодня")
+# print(my_list)
+# # Удаление объекта
+# my_list.remove(1)
+# print(my_list)
+# # Удаление по индексу
+# my_list.pop(0)
+# print(my_list)
+# # Очистка списка
+# my_list.clear()
+# print(my_list)
+# # Подчёт вхождений элемента в списке
+# val = my_list.count(1)
+# print(val)
+# # Сортировка списка
+# my_list1 = [1, 5, 6, 7, 455, 323, 11, 146]
+# my_list1.sort(reverse=False)
+# print(my_list1)
+# # Перевернуть список
+# my_list1.reverse()
+# print(my_list1)
+#
+# counter = 0
+# while counter < len(my_list1):
+#     val = my_list1[counter]
+#     if val == 323:
+#         print("Мы нашли число 323")
+#         break
+#     print(my_list1[counter])
+#     counter += 1

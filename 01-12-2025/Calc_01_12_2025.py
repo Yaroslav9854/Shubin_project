38d37adf16751dd289b555c4cd05f0bc6fc17962
Falaf1 <subinad53@gmail.com>
2025-12-01 21:13:46 +0500

    Сделано 5 практических заданий из 7.

---
+++ 01-12-2025/Calc_01_12_2025.py
@@ -0,0 +1,12 @@
+import math
+import sys
+from datetime import datetime
+
+
+pi = 3.141592
+
+# print(math.tau)
+# print(pi)
+# print(sys.modules)
+# Модули - это файлы с расширеннием для Python.
+# Пространство имён - это область, где находятся все имена объектов (пространство имён).

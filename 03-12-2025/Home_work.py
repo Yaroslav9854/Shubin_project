63ebe72c1b10cd1f2ba6c47834572f4de91a7b36
student <yra.shubin@gmail.com>
2025-12-04 21:18:44 +0500

    Функции

--- 03-12-2025/Home_work.py
+++ 03-12-2025/Home_work.py
@@ -1,47 +1,6 @@
 # Вывести на экран фигуры, заполненные звездочками.
 # Диалог с пользователем реализовать при помощи меню.
 
-# def print_figure(size):
-#     # Выводим верхнюю часть квадрата
-#     for i in range(size):
-#         # Выводим пробелы
-#         for j in range(i):
-#             print(" ", end="")
-#             # Выводим звездочки
-#         for k in range(size - i):
-#             print("*", end="")
-#         print()  # Переход на новую строку
-#
-#
-# def main():
-#     while True:
-#         print("\nМеню:")
-#         print("1. Нарисовать фигуру")
-#         print("2. Выход")
-#
-#         choice = input("Выберите пункт меню: ")
-#
-#         if choice == '1':
-#             try:
-#                 size = int(input("Введите размер фигуры (целое число должно быть больше 0): "))
-#                 if size > 0:
-#                     print_figure(size)
-#                 else:
-#                     print("Размер должен быть положительным числом!")
-#             except ValueError:
-#                 print("Ошибка: введите целое число!")
-#
-#         elif choice == '2':
-#             print("До свидания!")
-#             break
-#         else:
-#             print("Неверный выбор. Попробуйте еще раз.")
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
 def print_1(height):
     for i in range(height):
         print(' ' * i + '*' * (height - i))
@@ -57,18 +16,23 @@ def print_4(height):
 def print_5(height):
     print_3(height)
     print_4(height)
-# def print_6(height):
-#     for i in range(height // 2):
-#         print(' ' * i + '*' * (2 * (height // 2 - i) - 1))
-#     for i in range(height // 2):
-#         print(' ' * (height // 2 - i - 1) + '*' * (2 * i + 1))
-def print_7(height):
+def print_6(height):
     for i in range(height):
-        # Выводим пробелы для сдвига
+        if i < 5:
+            print("*" * (height // 2 - i - 1) + '*' * (2 * i + 1))
+
+# Вывести на экран фигуры, заполненные звездочками.
+# Диалог с пользователем реализовать при помощи меню.
+# Надо вывести два равносторонних треугольника в квадрате которые находятся один на левой, второй на правой стороне квадрата.
+# При этом одна вершина одного треугольника должна быть направлена на другую вершину второго треугольника.
+# Эти вершины соединяются в центре квадрата с левой и правой стороны.
+# Выполнить в виде одной функции.
+# def print_7(height):
+
+def print_8(height):
+    for i in range(height):
         print(' ' * (height - i - 1), end='')
-        # Выводим звездочки
         print('*' * (i + 1))
-    # def print_8(height):
 def print_9(height):
     for i in range(height):
         print('*' * (height - i) + ' ' * (i - 1))
@@ -82,7 +46,9 @@ while True:
     print("3. Равносторонний треугольник с основанием по верхнему краю")
     print("4. Равносторонний треугольник с основанием по нижнему краю")
     print("5. Равносторонние треугольники с основаниями по верхнему и нижнему краям")
-    # print("6. Равносторонние треугольники с основаниями по левым и правым краям")
+    print("6. Равносторонние треугольники с основаниями по левым и правым краям")
+    print("7. Равносторонний треугольник с основанием по левому краю")
+    print("8. Равносторонний треугольник с основанием по правому краю")
     print("9. Треугольник в верхнем левом углу")
     print("10. Треугольник в нижнем правом углу")
     print("0. Выход")
@@ -102,15 +68,15 @@ while True:
     elif choice == 5:
         height = int(input("Введите размер квадрата: "))
         print_5(height)
-    # elif choice == 6:
-    #     height = int(input("Введите размер квадрата: "))
-    #     print_6(height)
+    elif choice == 6:
+        height = int(input("Введите размер квадрата: "))
+        print_6(height)
     elif choice == 7:
         height = int(input("Введите размер квадрата: "))
         print_7(height)
-    # elif choice == 8:
-    #     height = int(input("Введите размер квадрата: "))
-    #     print_8(height)
+    elif choice == 8:
+        height = int(input("Введите размер квадрата: "))
+        print_8(height)
     elif choice == 9:
         height = int(input("Введите размер квадрата: "))
         print_9(height)
